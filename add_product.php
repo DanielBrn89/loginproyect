83053929a932c6f2c85a086e91b028380a92ba70
DanielBrn89 <168025488+DanielBrn89@users.noreply.github.com>
2025-04-10 18:58:13 -0600

    Delete add_product.php

--- add_product.php
+++
@@ -1,8 +0,0 @@
-<form action="save_product.php" method="POST">
-    <input type="text" name="name" placeholder="Nombre del producto" required>
-    <textarea name="description" placeholder="Descripción"></textarea>
-    <input type="number" name="quantity" placeholder="Cantidad" required>
-    <input type="number" name="price" placeholder="Precio" step="0.01" required>
-    <input type="hidden" name="user_id" value="1"> <!-- Ajustar según login -->
-    <button type="submit">Guardar producto</button>
-</form>
