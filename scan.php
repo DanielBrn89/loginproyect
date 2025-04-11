07973df8fe2cdc884d5bed723f8a7712593aa7b4
DanielBrn89 <168025488+DanielBrn89@users.noreply.github.com>
2025-04-10 18:57:46 -0600

    Delete scan.php

--- scan.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="register_scan.php">
-    <input type="text" name="product_id" placeholder="Escanea código..." autofocus>
-    <input type="hidden" name="user_id" value="1">
-    <button type="submit">Registrar escaneo</button>
-</form>
