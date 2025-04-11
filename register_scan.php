62542618bcd95297d6828b702c6dec58df922474
DanielBrn89 <168025488+DanielBrn89@users.noreply.github.com>
2025-04-10 18:57:32 -0600

    Delete register_scan.php

--- register_scan.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'db.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $stmt = $pdo->prepare("INSERT INTO qr_scans (product_id, user_id) VALUES (?, ?)");
-    $stmt->execute([$_POST['product_id'], $_POST['user_id']]);
-
-    echo "Escaneo registrado correctamente.";
-}
